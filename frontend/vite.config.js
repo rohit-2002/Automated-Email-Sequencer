6757d2621fc32876d77795d4fa5e3e392afef933
Rohit Baba <rohitbaba198@gmail.com>
2025-01-12 18:43:06 +0530

    chnages in vite.congig.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Bind to all available interfaces
+    port: process.env.PORT || 5173, // Use the PORT environment variable or fallback to 5173
+  },
+});
